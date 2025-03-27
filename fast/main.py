b04d7336ee110abb2e6238b89f7829cbbfd15ecb
Luis Mayorga <eduardo.mayorgab@gmail.com>
2025-03-27 15:13:57 -0600

    update y Delete

--- fast/main.py
+++ fast/main.py
@@ -102,26 +102,45 @@ def agregarUsuarios(usuario: modeloUsuario):
 
 
 #endpoit para actualizar un usuario existente
-@app.put("/usuarios/{id}", response_model=modeloUsuario, tags=['Operaciones CRUD'])
-def actualizarUsuario(id: int, usuarioactualizado: modeloUsuario):
-    for index, usr in enumerate (usuarios):
-        if usr ["id"]== id:
-            usuarios[index]=usuarioactualizado.model_dump()
-            return usuarios[index]
-        raise HTTPException (status_code=400, detail="no se encontro el usuario")
-        
-    
+@app.put("/usuarios/{id}", tags=['Operaciones CRUD'])
+def actualizarUsuario(id:int, usuario: modeloUsuario):
+    db=Session()
+    try:
+        usuarios = db.query(User).filter(User.id == id).first()
+        if not usuarios:
+            return JSONResponse(status_code=404, content={"message": "usuario no encontrado"})
+        db.query(User).filter(User.id == id).update(usuario.model_dump())
+        db.commit()
+
+        usuarioactualizado = db.query(User).filter(User.id == id).first()
+        return JSONResponse(content=jsonable_encoder(usuarioactualizado))
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500,
+                            content={"message":"error al actualizar usuario",
+                                    "excepcion": str(e)})
+    finally:
+        db.close()        
+
 #entpoit parae eliminar usuario por ID
 @app.delete("/usuarios/{id}", tags=['Operaciones CRUD'])
 def eliminarUsuario(id: int):
-    for usuario in usuarios:
-        if usuario["id"] == id:
-            usuarios.remove(usuario)
-            return {"mensaje": "usuario eliminado", "usuario": usuario}
-    
-    raise HTTPException(status_code=400, detail="el usuario no existe, no se puede eliminar")
-
-
+    db=Session()
+    try:
+        usuario = db.query(User).filter(User.id == id).first()
+        if not usuario:
+            return JSONResponse(status_code=404, content={"message": "usuario no encontrado"})
+        
+        db.query(User).filter(User.id == id).delete()
+        db.commit()
+        return JSONResponse(content={"message": "Usuario eliminado correctamente"})
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500,
+                            content={"message":"error al eliminar usuario",
+                                    "excepcion": str(e)})
+    finally:
+        db.close()
 
 
 """
